d484da2a93c1c08b1fc895bfaa9e4d85aa84cf21
Daniel A Ramirez <dr82913@srcs.loc>
2018-05-07 12:17:30 -0700

    Added more and will fix

---
+++ vanimation.js
@@ -0,0 +1,14 @@
+function Circle(xpos, ypos, xstep, ystep, color, id) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.xstep = xstep;
+    this.ystep = ystep;
+    this.id = id;
+    this.color = color;
+    var elem = document.getElementById(this.id);
+    this.render = function () {
+        elem.style.top = this.ypos + 'px';
+        elem.style.left = this.xpos + 'px';
+        elem.style.backgroundColor = color;
+    }
+}
