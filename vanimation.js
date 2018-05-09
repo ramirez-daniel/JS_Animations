35fdba724b8fb0e15a816db85649eeb308dbbfe8
Daniel A Ramirez <dr82913@srcs.loc>
2018-05-09 12:19:26 -0700

    Almost done

--- vanimation.js
+++ vanimation.js
@@ -1,4 +1,4 @@
-function Circle(xpos, ypos, xstep, ystep, color, id) {
+function Dot(xpos, ypos, xstep, ystep, color, id) {
     this.xpos = xpos;
     this.ypos = ypos;
     this.xstep = xstep;
@@ -10,5 +10,39 @@ function Circle(xpos, ypos, xstep, ystep, color, id) {
         elem.style.top = this.ypos + 'px';
         elem.style.left = this.xpos + 'px';
         elem.style.backgroundColor = color;
+    }
+}
+
+var dots = [];
+var numDots = 10;
+var container = document.getElementById("container");
+
+for(var i = 0; i < numDots; i++){
+    var elem = document.createElement("div");
+    elem.className = "dot";
+    elem.id = "dot" + i;
+    container.appendChild(elem);
+    dots[i] = new Dot(Math.random()*750,
+                       Math.random()*750,
+                       Math.random(),
+                       Math.random(),
+                       '#'+Math.floor(Math.random()*16777215).toString(16),
+                       elem.id);
+}
+
+var id = setInterval(frame, 5);
+
+function frame() {
+    for (var i = 0; i < dots.length; i++) {
+        if (dots[i].xpos > 750 || dots[i].xpos < 0) {
+            dots[i].xstep = -dots[i].xstep;
+        }
+        if (dots[i].ypos > 750 || dots[i].ypos < 0) {
+            dots[i].ystep = -dots[i].ystep;
+        }
+
+        dots[i].xpos = dots[i].xpos + dots[i].xstep;
+        dots[i].ypos = dots[i].ypos + dots[i].ystep;
+        dots[i].render();
     }
 }
